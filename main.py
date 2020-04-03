efca68adfd91c01caf9c193772f8fb3fdc501d34
mgiurca <mgiurca.mail@gmail.com>
2020-04-03 12:42:36 +0300

    adjusted face cascade - detect multiscale, min neighbours

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 import cv2
-
+import pickle
 cap = cv2.VideoCapture(0)
-faceCascade = cv2.CascadeClassifier("venv/Lib/site-packages/cv2/data/haarcascade_frontalface_alt2.xml")
+faceCascade = cv2.CascadeClassifier("venv/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
 rect_color = (255,0,0)
+ai = cv2.face.LBPHFaceRecognizer_create()
+ai.read("face_trainer.yml")
+
+with open("faces.rick", 'rb') as f:
+    dump = pickle.load(f) #type: dict
+    face_name = {v:k for k, v in dump.items()}
+
+print(face_name)
 while True:
     ret, frame = cap.read()
     grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(grayFrame)
-    for (x,y,w,h) in faces:
-        print(x,y,w,h)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), rect_color, 2)
+    faces = faceCascade.detectMultiScale(grayFrame, minNeighbors=3, scaleFactor=1.1)
 
+    for (x,y,w,h) in faces:
+        #print(x,y,w,h)
+        predict_face = grayFrame[y:y+h, x:x+w]
+        cv2.imshow("predict face", predict_face)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), rect_color, 2)
+        name, conf = ai.predict(predict_face)
+
+        if conf < 70:
+            print(x,y,w,h,"conf = ",conf, "person = ", face_name[name])
+        else:
+            print("unknown with conf = ", conf)
     cv2.imshow("webcam", frame)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-
 cap.release()
 cv2.destroyAllWindows()
--- process_faces.py
+++ process_faces.py
@@ -2,27 +2,50 @@ import os
 from PIL import Image, ImageDraw
 import numpy as np
 import cv2
+import pickle
 
-faceCascade = cv2.CascadeClassifier("venv/Lib/site-packages/cv2/data/haarcascade_frontalface_alt2.xml")
+
+faceCascade = cv2.CascadeClassifier("venv/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
 base_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(base_dir, 'Faces')
+faces_dict = {}
 train_faces = []
 train_labels = []
-print(images_dir)
+counter = 0
+size = (550, 550)
 
 for root, dirs, files in os.walk(images_dir):
     for file in files:
         if file.endswith("jpg"):
             path = os.path.join(root,file)
             label = os.path.basename(os.path.dirname(path))
-            #print(label, "@", path)
+            print(label, "@", path)
             image = Image.open(path).convert("L") # "L" pentru grayscale
             image_mat = np.array(image, "uint8")
             faces = faceCascade.detectMultiScale(image_mat)
+
+            while True:
+                for (x,y,w,h) in faces:
+                    cv2.imshow("face", image_mat[y:y+h, x:x+w])
+                if cv2.waitKey(20) & 0xFF == ord('q'):
+                    break
+
+
+            if label not in faces_dict:
+                faces_dict[label] = counter
+                counter -= -1
+
+            id_ = faces_dict[label]
             for (x,y,w,h) in faces:
                 face = image_mat[y:y+h, x:x+w]
                 train_faces.append(face)
-                train_labels.append(label)
+                train_labels.append(id_)
 
-#print(train_faces)
-#print(train_labels)
+# print(train_faces)
+print(faces_dict)
+with open("faces.rick", "wb") as f:
+    pickle.dump(faces_dict, f)
+
+reco = cv2.face.LBPHFaceRecognizer_create()
+reco.train(train_faces, np.array(train_labels))
+reco.save("face_trainer.yml")
